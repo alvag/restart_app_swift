ee93fda27dfdf5434e829059fc2e1178f2294550
Max Alva Garcia <alva85@gmail.com>
2023-06-09 06:52:07 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Max Alva on 9/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
