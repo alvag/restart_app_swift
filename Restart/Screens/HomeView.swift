848a13130e9994b8b47df2372343653f347946bb
Max Alva Garcia <alva85@gmail.com>
2023-06-09 15:16:36 -0500

    Animation

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct HomeView: View {
-    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+  @State private var isAnimating: Bool = false
+  
+  // MARK: - BODY
     var body: some View {
         VStack(spacing: 20) {
             
@@ -17,11 +20,17 @@ struct HomeView: View {
             Spacer()
             
             ZStack {
-                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.2)
-                Image("character-2")
-                    .resizable()
-                    .scaledToFit()
+              CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.2)
+              Image("character-2")
+                .resizable()
+                .scaledToFit()
                 .padding()
+                .offset(y: isAnimating ? 35 : -35)
+                .animation(
+                  .easeOut(duration: 4)
+                  .repeatForever()
+                  , value: isAnimating
+                )
             }
             
             // CENTER
@@ -38,7 +47,9 @@ struct HomeView: View {
             Spacer()
         
             Button(action: {
+              withAnimation {
                 isOnboardingViewActive = true
+              }
             }) {
                 Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                     .imageScale(.large)
@@ -50,6 +61,11 @@ struct HomeView: View {
             .buttonStyle(.borderedProminent)
             .buttonBorderShape(.capsule)
             .controlSize(.large)
+            .onAppear(perform: {
+              DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+              })
+            })
         }
     }
 }
